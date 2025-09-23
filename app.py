7f6d3c9587de6c9066921c7c9c4f72a6f4893e80
V-coder-cmd <v.amancio@aluno.senai.br>
2025-09-23 13:49:16 -0300

    Atulaziando chave secreta

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 # CRIANDO CHAVE SECRETA
-app.secret_key = "SLAIRMAO"
+app.secret_key = "8350e5a3e24c153df2275c9f80692773"
 
 
 @app.route("/")
