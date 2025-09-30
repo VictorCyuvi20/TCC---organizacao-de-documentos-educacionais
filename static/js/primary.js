6971f5cdb1f7b3dce04fe4b64af48745cc57c966
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-30 16:35:48 -0300

    fix: formatting

--- static/js/primary.js
+++ static/js/primary.js
@@ -1,12 +1,14 @@
-const goBackButton = document.querySelector('#go-back');
+document.addEventListener('DOMContentLoaded', function(){
+    const goBackButton = document.querySelector('#goback');
 
-if (goBackButton){
-    if (document.URL.at(-1) === '/'){
-        goBackButton.style.display = 'none';
-        return;
+    if (goBackButton){
+        if (document.URL.at(-1) === '/'){
+            goBackButton.style.display = 'none';
+            return;
+        };
+
+        goBackButton.addEventListener('onClick', function(){
+            window.history.back();
+        });
     };
-
-    goBackButton.addEventListener('onClick', function(){
-        window.history.back();
-    });
-};
+})
