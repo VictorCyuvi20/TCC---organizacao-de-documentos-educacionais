e8a622a7d4bc5663287192a766d263770f55a4bf
v__cod)_ <v.amancio@aluno.senai.br>
2025-10-06 09:29:56 -0300

    Reparo no banco de dados

--- DATA/migration/banco_de_dados.sql
+++ DATA/migration/banco_de_dados.sql
@@ -1,61 +1,62 @@
-create database if not exists db_tcc;
-use db_tcc;
+CREATE DATABASE IF NOT EXISTS db_tcc;
+USE db_tcc;
 
-create table if not exists tb_user(
-	id_user int primary key auto_increment,
-    name char(40) not null,
-    password varchar(255) not null,
-    email varchar(50) not null,
-    profile_type char(15) not null
+CREATE TABLE IF NOT EXISTS tb_user (
+    id_user INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    profile_type VARCHAR(15) NOT NULL
 );
 
-create table if not exists tb_document(
-	id_document int primary key auto_increment,
-    name char(40) not null,
-    descripition char(255) not null,
-    category char(15) not null,
-    image text not null,
-    amount int not null
+CREATE TABLE IF NOT EXISTS tb_document (
+    id_document INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    doc_description VARCHAR(255) NOT NULL,
+    category VARCHAR(15) NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    amount INT NOT NULL
 );
 
-create table if not exists tb_historic(
-	id_historic int primary key auto_increment,
-    -- Chaves Estrangeiras
-    id_user int,
-    id_document int,
-    foreign key (id_user) references tb_user(id_user),
-    foreign key (id_document) references tb_document(id_document),
-    -- Data e Hora
-    date_and_time DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS tb_historic (
+    id_historic INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_document INT,
+    FOREIGN KEY (id_user) REFERENCES tb_user(id_user),
+    FOREIGN KEY (id_document) REFERENCES tb_document(id_document),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists tb_request(
-	id_request int primary key auto_increment,
-    -- Chaves Estrangeiras
-    id_user int,
-    id_document int,
-    foreign key (id_user) references tb_user(id_user),
-    foreign key (id_document) references tb_document(id_document)
+CREATE TABLE IF NOT EXISTS tb_request (
+    id_request INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_document INT,
+    FOREIGN KEY (id_user) REFERENCES tb_user(id_user),
+    FOREIGN KEY (id_document) REFERENCES tb_document(id_document),
+    status VARCHAR(15) DEFAULT 'solicitado'
 );
 
-create table tb_user_document(
-	id_user_document int primary key auto_increment,
-    -- Chaves Estrangeiras
-    id_user int,
-    id_document int,
-	foreign key (id_user) references tb_user(id_user),
-    foreign key (id_document) references tb_document(id_document)
+CREATE TABLE IF NOT EXISTS tb_user_document (
+    id_user_document INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_document INT,
+    FOREIGN KEY (id_user) REFERENCES tb_user(id_user),
+    FOREIGN KEY (id_document) REFERENCES tb_document(id_document),
+    CONSTRAINT unique_user_document UNIQUE(id_user, id_document)
 );
 
-select * from tb_user;
-insert into tb_user(name, password, email, profile_type) values("zacajaca21", "12345678", "seuemail1@gmail.com", "master");
-insert into tb_user(name, password, email, profile_type) values("Cyuvi", "12345678", "seuemail2@gmail.com", "common");
-insert into tb_user(name, password, email, profile_type) values("Skuar", "12345678", "seuemail3@gmail.com", "commun");
+-- Exemplo de inserções de dados
 
-select * from tb_document;
-insert into tb_document(name, descripition, category, image, amount) values("Prova de Matemática", "Esta prova possui 10 questões sobre matemática avançada", "matemática", "../../static/images/imagem_exemplo1.jpg", 10);
-insert into tb_document(name, descripition, category, image, amount) values("Prova de Química", "Esta prova possui 10 questões sobre química avançada", "química", "../../static/images/imagem_exemplo3.jpg", 15);
-insert into tb_document(name, descripition, category, image, amount) values("Prova de Biologia", "Esta prova possui 10 questões sobre biologia avançada", "biologia", "../../static/images/imagem_exemplo3.jpg", 20);
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca21", "12345678", "seuemail1@gmail.com", "master");
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca22", "12345678", "seuemail2@gmail.com", "common");
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca23", "12345678", "seuemail3@gmail.com", "common");
 
-select * from tb_user inner join tb_document ON tb_user.id_user = tb_document.id_document
-inner join tb_request ON tb_document.id_document = tb_request.id_document;
+INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("prova de matematica", "Esta prova possui 10 questões sobre matemática avançada", "matematica", "../../static/images/imagem_exemplo1.jpg", 10);
+INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("prova de quimica", "Esta prova possui 10 questões sobre química avançada", "quimica", "../../static/images/imagem_exemplo3.jpg", 15);
+INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("prova de biologia", "Esta prova possui 10 questões sobre biologia avançada", "biologia", "../../static/images/imagem_exemplo3.jpg", 20);
+
+-- Corrigindo a junção entre as tabelas
+SELECT * 
+FROM tb_user 
+INNER JOIN tb_request ON tb_user.id_user = tb_request.id_user 
+INNER JOIN tb_document ON tb_request.id_document = tb_document.id_document;
