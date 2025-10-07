a5712f09da1f50d11298c08ba1d6a7484b01d2be
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-10-07 14:37:46 -0300

    feat: navbar's html, css classes, and javascript

--- static/js/primary.js
+++ static/js/primary.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const goBackButton = document.querySelector('#goback');
+    const navButtons = document.querySelectorAll('.navbar-option')
 
     if (goBackButton){
         if (document.URL.at(-1) === '/'){
@@ -11,4 +12,13 @@ document.addEventListener('DOMContentLoaded', function(){
             window.history.back();
         });
     };
-})
+
+    navButtons.forEach(button => {
+        if (!button.tagName == 'A') return;
+
+        if (button.pathname === window.location.pathname){
+            button.classList.add('selected');
+            console.log(button.pathname, window.location.pathname)
+        };
+    });
+});
