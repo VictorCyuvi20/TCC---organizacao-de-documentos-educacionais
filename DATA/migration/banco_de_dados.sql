5f4295f1fef25624800cdf220560d08405b11fae
zacajaca21 <zacajaca21@gmail.com>
2025-10-14 17:26:05 -0300

    add: correção de erro de escrita na tabela do banco de dados tb_notification

--- DATA/migration/banco_de_dados.sql
+++ DATA/migration/banco_de_dados.sql
@@ -45,19 +45,26 @@ CREATE TABLE IF NOT EXISTS tb_user_document (
     CONSTRAINT unique_user_document UNIQUE(id_user, id_document)
 );
 
+CREATE TABLE IF NOT EXISTS tb_notification(
+	id_notification INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_document INT,
+	FOREIGN KEY (id_user) REFERENCES tb_user(id_user),
+    FOREIGN KEY (id_document) REFERENCES tb_document(id_document)
+);
+
 -- Exemplo de inserções de dados
 
-INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca21", "12345678", "seuemail1@gmail.com", "master");
-INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca22", "12345678", "seuemail2@gmail.com", "common");
-INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca23", "12345678", "seuemail3@gmail.com", "common");
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca21", "123456789", "seuemail1@gmail.com", "master");
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca22", "123456789", "seuemail2@gmail.com", "common");
+INSERT INTO tb_user (name, password, email, profile_type) VALUES ("zacajaca23", "123456789", "seuemail3@gmail.com", "common");
 
 INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("Prova de Matemática", "Esta prova possui 10 questões sobre matemática avançada", "matematica", "../../static/images/imagem_exemplo1.jpg", 10);
 INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("Prova de Química", "Esta prova possui 10 questões sobre química avançada", "quimica", "../../static/images/imagem_exemplo2.jpg", 15);
 INSERT INTO tb_document (name, doc_description, category, image, amount) VALUES ("Prova de Biologia", "Esta prova possui 10 questões sobre biologia avançada", "biologia", "../../static/images/imagem_exemplo3.jpg", 20);
 
 -- Corrigindo a junção entre as tabelas
-SELECT * 
-FROM tb_user 
+SELECT * FROM tb_user
 INNER JOIN tb_request ON tb_user.id_user = tb_request.id_user 
 INNER JOIN tb_document ON tb_request.id_document = tb_document.id_document;
 
